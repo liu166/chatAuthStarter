a4c3f42f85a4616aa30efe72b039b23cfde485eb
coolBoy <291816874@qq.com>
2026-01-28 10:48:18 +0800

    commit

--- src/main/java/com/rag/common/config/AuthProperties.java
+++ src/main/java/com/rag/common/config/AuthProperties.java
@@ -3,17 +3,15 @@ package com.rag.common.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "auth")
 public class AuthProperties {
-    private String jwtSecret = "";
+    private String jwtSecret = "auth-secret-auth-secret-auth-secret-1234";
     private int expireHours = 2;
     private List<String> excludes = new ArrayList<>();
 }
